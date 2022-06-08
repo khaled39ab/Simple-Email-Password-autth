3a702ab4b8bb46912f1d0b50b1a2966d65ec9f6e
khaled39ab <khaled39ab@gmail.com>
2022-06-08 17:46:07 +0600

    creating form

--- src/App.js
+++ src/App.js
@@ -5,9 +5,27 @@ import { getAuth } from "firebase/auth";
 const auth = getAuth(app);
 
 function App() {
+  const handleEmail = e =>{
+    console.log(e.target.value);
+  }
+
+  const handlePassword = e =>{
+    console.log(e.target.value);
+  }
+
+  const handleFormSubmit = e =>{
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
-      
+      <form onSubmit={handleFormSubmit}>
+        <input onBlur={handleEmail} type="email" name="" id="" />
+        <br />
+        <input onBlur={handlePassword} type="password" name="" id="" />
+        <br />
+        <input type="submit" value="Log In" />
+      </form>
     </div>
   );
 }
