9a91e312b1d4ac0ed8ccc69b3a1d324874dbe301
khaled39ab <khaled39ab@gmail.com>
2022-06-08 18:59:50 +0600

    create account

--- src/App.js
+++ src/App.js
@@ -1,31 +1,74 @@
 import './App.css';
 import app from './firebase.init';
-import { getAuth } from "firebase/auth";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { Button, Form } from 'react-bootstrap';
+import { useState } from 'react';
+// import Button from 'react-bootstrap/Button';
+// import Form from 'react-bootstrap/Form'
 
 const auth = getAuth(app);
 
 function App() {
-  const handleEmail = e =>{
-    console.log(e.target.value);
+  const [validated, setValidated] = useState(false)
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('')
+
+  const handleEmail = e => {
+    setEmail(e.target.value);
   }
 
-  const handlePassword = e =>{
-    console.log(e.target.value);
+  const handlePassword = e => {
+    setPassword(e.target.value);
   }
 
-  const handleFormSubmit = e =>{
+  const handleFormSubmit = e => {
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+      return;
+    }
+
+    setValidated(true);
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then(res => console.log(res.user))
+      .catch(err => console.error(err))
     e.preventDefault();
   }
 
   return (
-    <div className="App">
-      <form onSubmit={handleFormSubmit}>
-        <input onBlur={handleEmail} type="email" name="" id="" />
-        <br />
-        <input onBlur={handlePassword} type="password" name="" id="" />
-        <br />
-        <input type="submit" value="Log In" />
-      </form>
+    <div>
+      <div className='registration w-50 mx-auto m-5 border p-3'>
+        <h2 className='text-success'>Please Registration</h2>
+        <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control onBlur={handleEmail} type="email" placeholder="Enter email" required />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid email.
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control onBlur={handlePassword} type="password" placeholder="Password" required />
+            <Form.Control.Feedback type="invalid">
+              Please provide a valid password.
+            </Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
